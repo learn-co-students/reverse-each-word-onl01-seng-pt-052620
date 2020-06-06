c5deeccdb602f0e7ad45ef266f74e7dfa1526325
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-06 00:04:32 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  sentence1.split.collect {|word| word.reverse}.join(" ")
 end
-end
+
 
 def reverse_each_word(sentence2)
   sentence2.reverse
